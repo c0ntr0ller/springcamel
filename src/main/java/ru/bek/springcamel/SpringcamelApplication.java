721f6a380d0d3a61741cdf39dc13551493182d08
c0ntr0ller <bek.andrey@gmail.com>
2018-04-24 19:56:17 +0700

    init

---
+++ src/main/java/ru/bek/springcamel/SpringcamelApplication.java
@@ -0,0 +1,12 @@
+package ru.bek.springcamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcamelApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/bek/springcamel/SpringcamelApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.bek.springcamel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcamelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
